328ebe6c13b7c0e8c2ffcc069387b1350bf0a45b
Rich-Taylor <richt-service@runbox.com>
2013-06-28 14:58:34 -0700

    Playing around

--- document.rb
+++ document.rb
@@ -15,22 +15,21 @@ class Document
 
   end
 
-  def word_usage
+  def word_frequency
     @word_use = Hash.new
-    words.each do |w|
-      if @word_use[w] == nil
-        @word_use[w] = 1
-      else
-        @word_use[w] += 1
-      end
-    end
+    words.each { |w| @word_use[w] = if @word_use[w] == nil then
+                                      1
+                                    else
+                                      @word_use[w] + 1
+                                    end
+    }
     @word_use
   end
 end
 
 doc = Document.new( 'title', 'author', 'now is the time for all good men to come to the aid of their country')
 
-h = doc.word_usage
+h = doc.word_frequency
 
 sorted_keys = h.keys.sort
 
